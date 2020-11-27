e2cae382e147e283e0a547e8cf7aa6cb282a6f42
hanliutong <hanliutong@gmail.com>
2020-11-27 19:19:32 +0800

    testset

--- camera/urls.py
+++ camera/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cube', views.cube, name='cube'),
     path('initState', views.initState, name='initState'),
+    path('testset', views.testset, name='testset'),
 ]
